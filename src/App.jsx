5cc350571c189ba6edbda01c0d0e87b69c1da698
Tahir Dincer <coskun.dincer2014@gmail.com>
2025-02-13 23:41:31 +0300

    ' Logic implemented. '

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import img from "./img/Spain-flag.jpg";
 import spanishNumbers from "./SpanishNumbers.js";
 const App = () => {
   const [numbers, setNumbers] = React.useState(spanishNumbers); // Numbers- Spanish Numbers in "text" type
-  const [randomizedNumber, setRandomizedNumber] = React.useState(0); // So this is the main number. Student can say this number.
-  const [on, isOn] = React.useState(false);
-  let randomNumber;
-  const showNumber = () => {
-    randomNumber = Math.floor(Math.random() * 99 + 1);
-    setRandomizedNumber((prevState) => numbers[randomNumber].spanish);
+  // So this is the main number. Student can say this number.
+  const [random, setRandom] = React.useState(0);
+  const [on, setOn] = React.useState(false);
+
+  const generateNumber = () => {
+    setRandom((prevState) => Math.floor(Math.random() * 99 + 1));
   };
-  return <h1 onClick={showNumber}>{randomizedNumber}</h1>;
+
+  const toggleNumber = () => {
+    setOn((prevState) => !prevState);
+  };
+
+  return (
+    <main className="numbers__container">
+      <img src={img} alt="Spanish flag" />
+      <h1>{numbers[random].number}</h1>
+
+      <div className="numbers__buttons">
+        <button onClick={generateNumber}>Numara Ver</button>
+        <button onClick={toggleNumber}>Bu Neydi Şimdi?</button>
+      </div>
+      <h2 className={on ? "on" : "off"}> {numbers[random].spanish}</h2>
+    </main>
+  );
 };
 
 export default App;
