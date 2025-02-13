a5b4998e21b9e5ccb72758d571928ccb75a40980
Tahir Dincer <coskun.dincer2014@gmail.com>
2025-02-13 22:54:58 +0300

    ' App logic implemented '

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import spanishNumbers from "./SpanishNumbers.js";
 const App = () => {
-  const [numbers, setNumbers] = React.useState(spanishNumbers);
+  const [numbers, setNumbers] = React.useState(spanishNumbers); // Numbers- Spanish Numbers in "text" type
+  const [randomizedNumber, setRandomizedNumber] = React.useState(0); // So this is the main number. Student can say this number.
   const [on, isOn] = React.useState(false);
-
-  return <h1>Hello, World</h1>;
+  let randomNumber;
+  const showNumber = () => {
+    randomNumber = Math.floor(Math.random() * 99 + 1);
+    setRandomizedNumber((prevState) => numbers[randomNumber].spanish);
+  };
+  return <h1 onClick={showNumber}>{randomizedNumber}</h1>;
 };
 
 export default App;
